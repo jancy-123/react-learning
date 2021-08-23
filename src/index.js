b777fa870a5bc04e8a1ac580b244234f316d8151
jancyrani <jancyrani@10decoders.in>
2021-08-23 19:52:38 +0530

    code has been changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
 import './index.css';
  import App from './App'
 
